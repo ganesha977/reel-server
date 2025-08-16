201716c6b0920f821ab3ad5bb9a18c2cdf3023b5
ganesha977 <rr1189183@gmail.com>
2025-08-16 22:20:31 +0530

    last

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const port = 7777;
 const corsOptions = {
   origin: [
     "http://localhost:5173", // Local dev
-    "https://social-mediagram.netlify.app/", // deployed frontend
+    "https://social-mediagram.netlify.app", // deployed frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
